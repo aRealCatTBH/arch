1003b780faaa686e21b4e330c28a4b1298e196d8
French Cat <89495053+French-Cat@users.noreply.github.com>
2022-12-28 15:14:02 -0500

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,1 +1,21 @@
-arch="$(uname -m)";if [ "$arch" = "x86_64" ];then arch="amd64";elif [ "$arch" = "armv7l" ];then arch="arm";elif [ "$arch" = "aarch64" ];then arch="arm64";else echo "Unsupported arch: $arch">&2;fi;if getconf GNU_LIBC_VERSION>/dev/null;then libc_postfix="";elif [ "$(ldd 2>&1|head -c 9)" = "musl libc" ];then libc_postfix="-musl";elif [ "$(find /lib/libc.musl*|wc -l)" ];then libc_postfix="-musl";else echo "Unsupported libc">&2;fi;echo "$arch$libc_postfix"
+arch = "$(uname -m)";
+if["$arch" = "x86_64"];
+then arch = "amd64";
+elif["$arch" = "armv7l"];
+then arch = "arm";
+elif["$arch" = "aarch64"];
+then arch = "arm64";
+else
+echo "Unsupported arch: $arch" > &2;
+fi;
+if getconf
+  GNU_LIBC_VERSION > /dev / null;
+then libc_postfix = "";
+elif["$(ldd 2>&1|head -c 9)" = "musl libc"];
+then libc_postfix = "-musl";
+elif["$(find /lib/libc.musl*|wc -l)"];
+then libc_postfix = "-musl";
+else
+echo "Unsupported libc" > &2;
+fi;
+echo "$arch$libc_postfix"
